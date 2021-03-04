c33257ad7c2289916da0e60db315be53fe914f2c
Michael Schmidt <michael@dedesignworks.com>
2021-03-04 12:20:41 -0600

    Add depdency.toml

--- bldr/dep/__init__.py
+++ bldr/dep/__init__.py
@@ -2,3 +2,10 @@
 Dependency fetchers
 
 """
+import toml
+
+import bldr
+from bldr.environment import Environment
+
+def config(ctx: Environment = Environment()):
+    return toml.load(f"{ctx.dotbldr_path}/dependency.toml")
