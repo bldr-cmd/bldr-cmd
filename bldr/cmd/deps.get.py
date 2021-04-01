1e7aa6e4d64cbedeb176e61bf7f722800a753a48
Michael Schmidt <michael@dedesignworks.com>
2021-04-01 14:45:56 -0500

    Update lock files

--- bldr/cmd/deps.get.py
+++ bldr/cmd/deps.get.py
@@ -9,14 +9,14 @@ from git.objects.submodule.root import RootUpdateProgress
 
 import bldr
 import bldr.gen.render
-
 from git import Repo
-
-dotbldr_path = os.path.join(os.path.abspath(os.path.dirname(bldr.__file__)), "dotbldr")
+import click
 
 from bldr.cli import pass_environment
 from bldr.gen.render import render
-import click
+import bldr.dep.env
+
+dotbldr_path = os.path.join(os.path.abspath(os.path.dirname(bldr.__file__)), "dotbldr")
 
 class DepsUpdateProgress(RootUpdateProgress):
     def __init__(self, ctx: Environment):
@@ -46,15 +46,22 @@ def cli(ctx):
     
     repo = Repo(git_path)
     
-    submodules = {name: dep for (name, dep) in ctx.env['dep']['lock'].items() if dep['type'] == 'git'}
+    config = {name: dep for (name, dep) in ctx.env['dep']['config'].items() if dep['type'] == 'git'}
+    lockfile = {name: dep for (name, dep) in ctx.env['dep']['lock'].items() if dep['type'] == 'git'}
+
+    # Remove deps not in the config file
+    lockfile = {name: dep for (name, dep) in lockfile.items() if name in config}
+
+    # Add an new deps from config file
+    lockfile.update(config)
 
     ctx.log("Add missing git modules")
-    for (subname, submod) in submodules.items():
-        module_path = git_path / "modules" / submod['path']
+    for (subname, lock_info) in lockfile.items():
+        module_path = git_path / "modules" / lock_info['path']
         if not module_path.exists():
-            ctx.log(f"submodule create {subname} {submod['path']} {submod['url']}")
-            #repo.create_submodule(subname, submod['path'], url=submod['url'], no_checkout=True)
-            output = repo.git.submodule('add', submod['url'], submod['path'])
+            ctx.log(f"submodule create {subname} {lock_info['path']} {lock_info['url']}")
+            #repo.create_submodule(subname, lock_info['path'], url=lock_info['url'], no_checkout=True)
+            output = repo.git.submodule('add', lock_info['url'], lock_info['path'])
             ctx.log(output)
 
     ctx.log("Updating git modules")
@@ -64,7 +71,11 @@ def cli(ctx):
     #repo.submodule_update(init=True, recursive=True, progress=progress)
     for submodule in repo.submodules:
         sha = submodule.hexsha
-        lock_info = submodules[submodule.name]
+        
+        lock_info['sha'] = sha
+
+        lock_info = lockfile[submodule.name]
+
         if 'branch' in lock_info:
             branch = lock_info['branch']
             sub_path = ctx.proj_path / lock_info['path']
@@ -73,3 +84,5 @@ def cli(ctx):
             ctx.log(f"Setting {submodule.name} {submodule.branch} {submodule.hexsha}")
             subrepo.git.checkout(branch)
             subrepo.git.reset('--hard', sha)
+
+    bldr.dep.env.save_lock(ctx.dotbldr_path, lockfile)
--- bldr/dep/env.py
+++ bldr/dep/env.py
@@ -7,5 +7,12 @@ def default(dotbldr_path: str) -> dict:
         'config': toml.load(f"{dotbldr_path}/dependency.toml"),
         'lock': toml.load(f"{dotbldr_path}/dependency.lock.toml")
     }
-    
     return dep
+
+def save_lock(dotbldr_path: str, lock_env: dict):
+    with open(f"{dotbldr_path}/dependency.lock.toml", 'w') as toml_file:
+        return toml.dump(lock_env, toml_file)
+
+def save_config(dotbldr_path: str, config_env: dict):
+    with open(f"{dotbldr_path}/dependency.toml", 'w') as toml_file:
+        return toml.dump(config_env, toml_file)
