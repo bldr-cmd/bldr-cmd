28567c05e0066e64392c51cc8b2033276b0eca42
Michael Schmidt <michael@dedesignworks.com>
2021-04-02 08:52:59 -0500

    Release 0.9

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(name='bldr',
-      version='0.8',
+      version='0.9',
       description='bldr framework',
       url='http://github.com/bldr/bldr_cmd',
       author='Michael Schmidt',
