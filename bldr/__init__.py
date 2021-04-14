b3d188614ff6ce13252631a0adfed72577fd5d6b
Michael Schmidt <michael@dedesignworks.com>
2021-04-14 16:14:29 -0500

    Remove dead code

--- bldr/__init__.py
+++ bldr/__init__.py
@@ -1,17 +1,1 @@
-import os
 
-def push_dir(new_dir: str):
-    original_cwd = os.path.abspath(os.path.curdir)
-    os.chdir(new_dir)
-    return original_cwd
-
-def proj_path():
-    return os.path.abspath( os.path.join(dotbldr_path(), ".." ))
-
-def dotbldr_path():
-    if os.path.exists(".bldr"):
-        return os.path.abspath(".bldr")
-
-def ensure_dir(path: str):
-    if not os.path.exists(path):
-        os.makedirs(path)
