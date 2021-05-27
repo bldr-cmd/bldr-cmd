c4acbbbb0eb4da095b1c1b168d01e099d1901499
AustinPipkins <akpvmr@umsystem.edu>
2021-05-27 15:53:32 -0500

    unit test for new :))))

---
+++ spec/new_spec.sh
@@ -0,0 +1,27 @@
+
+Describe 'bldr new'                                                                                           
+  Include spec/venv_inc
+  setup() {  
+    setup_w_bldr
+    create_git
+  }
+  cleanup() {  
+    cleanup_dir
+  }
+  
+
+  BeforeEach 'setup'
+  AfterEach 'cleanup'
+                                                                                       
+  It 'Calls bldr new, should add dependency'
+    git init > /dev/null 2>&1
+    bldr init > /dev/null 2>&1
+    When call bldr new https://github.com/microsoft/playwright-test > /dev/null 2>&1
+    The path ./playwright-test/ should be exist
+
+  End
+
+
+
+  
+End   
