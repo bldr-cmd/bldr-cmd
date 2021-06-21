5612f59e4d23ad45219a3d4c77bef472d5caf0c6
Michael Schmidt <michael@dedesignworks.com>
2021-06-21 11:18:29 -0500

    Set execute bit on git hooks

--- bldr/dep/__init__.py
+++ bldr/dep/__init__.py
@@ -6,9 +6,8 @@ import os
 import platform
 import shutil
 
-from bldr.gen.render import CopyTemplatesRender
+from bldr.gen.render import CommonRender
 from bldr.environment import Environment
-
 
 def sync_githooks(ctx: Environment) -> None:
     git_path = ctx.proj_path / ".git"
@@ -16,7 +15,15 @@ def sync_githooks(ctx: Environment) -> None:
         # Nothing to do
         return
 
-    copy_render = CopyTemplatesRender(ctx, True)
+    copy_render = CopyHooksRender(ctx, True)
     git_hooks_path = git_path / "hooks"
     copy_render.walk(ctx.proj_path / ".githooks", git_hooks_path)
-    
+
+class CopyHooksRender(CommonRender):
+    def render(self, source_path: str, destination_path: str):
+        # if the destination does not exist, just copy the file
+        self.ctx.vlog(f"Copying {source_path} -> {destination_path}")
+        shutil.copy(source_path, destination_path)
+        # Make sure we have the execute bit set
+        os.chmod(destination_path, 0o744)
+        return True
